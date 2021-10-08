d3f70e05076582e735f2e4337e6238a60a0d7df0
Edoardo <ercolino.edoardo@gmail.com>
2021-10-08 15:03:07 +0200

    Scaffolding e conclusione snack 1

---
+++ Snack 1/index.php
@@ -0,0 +1,35 @@
+<!-- ## Snack 1
+1) Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+2) Stampiamo a schermo tutte le partite con questo schema. 
+es: Olimpia Milano - Cantù | 55-60 -->
+
+<?php 
+    // creo l'array
+    $partite = [
+        [
+            'teamCasa' => 'Olimpia Milano',
+            'teamOspiti' => 'Cantù',
+            'puntiCasa' => 55,
+            'puntiOspiti' => 60
+        ],
+        [
+            'teamCasa' => 'Roma',
+            'teamOspiti' => 'Bari all-star',
+            'puntiCasa' => 50,
+            'puntiOspiti' => 70
+        ],  
+        [
+            'teamCasa' => 'Palermo',
+            'teamOspiti' => 'Lecce',
+            'puntiCasa' => 78,
+            'puntiOspiti' => 42
+        ],
+    ];
+
+    // effettuo ciclo for 
+    for ($i = 0; $i < count($partite); $i++) {
+        // ricordarsi che il punto sostituisce il + 
+        echo $partite[$i]['teamCasa'] . ' ' . '-' . ' ' . $partite[$i]['teamOspiti'] . ' ' . '|' . ' ' . $partite[$i]['puntiCasa'] . '-' . $partite[$i]['puntiOspiti'] . '<br/>' ;
+    };
+?>
---
+++ Snack 2/index.php
@@ -0,0 +1,15 @@
+<!-- ## Snack 2
+1) Passare come parametri GET name, mail e age 
+2) verificare (cercando i metodi che non conosciamo nella documentazione) che:
+    a) name sia più lungo di 3 caratteri,
+    b) che mail contenga un punto e una chiocciola 
+    c) che age sia un numero.
+3) se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+<?php 
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $age = $_GET['age'];
+
+    
+?>
