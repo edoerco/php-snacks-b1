cf9920ff9abc11ae5877d6ec8b06e0605c2a6d75
Edoardo <ercolino.edoardo@gmail.com>
2021-10-08 15:23:16 +0200

    Conclusione snack2

--- Snack 2/index.php
+++ Snack 2/index.php
@@ -11,5 +11,12 @@
     $mail = $_GET['mail'];
     $age = $_GET['age'];
 
-    
+    // 'strpos()' controlla se ci sta l'indice dell'elemento cercato all'interno della stringa.    
+    if (strlen($name) > 3 && strpos($mail, '@') && strpos($mail, '.') && is_numeric($age)) {
+        echo 'Accesso riuscito';
+    } 
+    else
+    {
+        echo 'Accesso negato';
+    }
 ?>
